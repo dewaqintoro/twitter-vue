66734f37ea6488873c5e5573844ab6889f3a3f60
dewaqintoro <dewaqintoro@gmail.com>
2021-04-01 10:27:56 +0700

    initial setup

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app" class="flex container h-screen w-full">
+    <!-- side nav -->
+    <div class="w-1/5 border-r border-lighter px-8 py-2">
+      <button class="h-12 w-12 hover:bg-lightblue text-3xl text-blue">
+        <i class="fab fa-twitter"></i>
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'app',
+  components: {
+  },
+}
+</script>
